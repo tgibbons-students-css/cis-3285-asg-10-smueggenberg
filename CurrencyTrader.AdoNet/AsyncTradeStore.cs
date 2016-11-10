17f7632d9499e9efc420602c7e0a9d8f771b7787
smueggenberg <smueggenberg@css.edu>
2016-11-10 11:39:15 -0600

    Assignment setup with AsyncTradeStore

---
+++ CurrencyTrader.AdoNet/AsyncTradeStore.cs
@@ -0,0 +1,18 @@
+﻿using CurrencyTrader.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyTrader.AdoNet
+{
+    class AsyncTradeStore : ITradeStorage
+    {
+        public void Persist(IEnumerable<TradeRecord> trades)
+        {
+            // Save the trades to the database
+            throw new NotImplementedException();
+        }
+    }
+}
